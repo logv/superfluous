719fb4b15267b6025adf5dc1adc3cde6ab8435db
okay <okay.zed@gmail.com>
2013-12-01 14:20:01 -0800

    removing unecessary files in default app

--- bin/default/config/config.js
+++ bin/default/config/config.js
@@ -11,35 +11,12 @@ module.exports = {
   superuser: {
     "admin" : true
   },
+  backend: {
 
-  // setting to true will make the UDP collector and the web server run as
-  // different processes, for stability reasons
-  separate_services: false,
+  },
   http_port: 3000,
   https_port: 3443,
   max_http_sockets: 1000,
   max_https_sockets: 1000,
-  require_https: true,
-  backend: {
-    driver: "mongo"
-  },
-  hostname: "localhost",
-  no_api_auth: false, // require user authentication to use API endpoint
-  google_auth: {
-    enabled: false,
-    // require_domain: 'my_custom_domain.com',
-    require_domain: false,
-    // authorized_users: { 
-    //  'you@your_google_domain.com': true,
-    //  'your_friend@her_google_domain.com' : true
-    // }
-    authorized_users: false
-  },
-  behind_proxy: false,
-  rss_feed: {
-    url: null
-  },
-  // This is the default max data size of the collection. Each dataset will
-  // only grow to this size and no further, ideally
-  default_max_dataset_size: 1024 * 1024 * 100 // 100 MB
+  require_https: true
 };
--- bin/default/config/override.js
+++ bin/default/config/override.js
@@ -2,9 +2,6 @@ module.exports = {
   sockets: true,
   behind_proxy: true,
   require_https: false,
-  separate_services: false,
-  no_api_auth: true,
   http_port: 3300,
-  https_port: 3343,
-  default_max_dataset_size: 1024 * 1024 * 100
+  https_port: 3343
 };
