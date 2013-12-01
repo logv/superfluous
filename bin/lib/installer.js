cc36d9f510cd6b94fbe49ff8b566729c8fc90aef
okay <okay.zed@gmail.com>
2013-12-01 14:35:06 -0800

    updating installed script (remove excess functionality) (0.0.3)

--- bin/lib/installer.js
+++ bin/lib/installer.js
@@ -31,55 +31,45 @@ function get_app_dir() {
 }
 
 function create_superfluous_app(app_name) {
+  console.log(app_name, typeof app_name);
+  if (typeof app_name !== "string") {
+    console.log("You should specify an app name: superfluous create <app>");
+    return;
+  }
+
   // First check to see if current dir is a superfluous app
   var app_dir = get_app_dir();
   var dest_dir = path.resolve(cwd, app_name);
 
   // if it is, throw an error. Easy way is to look for routes.json file
   if (app_dir) {
-    console.log(program.dir, "is already a superfluous app. Did you mean to update?");
+    console.log(program.dir, "is already a superfluous app!");
   } else {
     console.log("Initializing a new superfluous app in", app_name);
-    fse.copy(base_dir, app_name, function(err) {
+    fse.copy(base_dir, dest_dir, function(err) {
       if (err) {
         console.log(err);
       } else {
-        exec("npm link superfluous");
+        console.log("Linking superfluous bundle into node_modules");
+        exec("npm link superfluous", { cwd: dest_dir });
+        console.log("Updating app variables in", dest_dir);
+        var cmd = "sed -i 's/APPNAME/" + app_name + "/g' package.json";
+        exec(cmd, { cwd: dest_dir });
+        // Linking 
       }
     });
   }
 }
 
-function init_superfluous_app() {
-  // First check to see if current dir is a superfluous app
-  var app_dir = get_app_dir();
-  var dest_dir = path.resolve(cwd, program.dir || ".");
-
-  create_superfluous_app(program.dir || ".");
-}
-
 module.exports = {
   run: function() {
     program
-      .option('-d, --dir [dir]', 'set the superfluous directory [dir]', '.')
       .version('0.0.1');
 
     program
       .command('create')
       .description('create a new superfluous app')
       .action(create_superfluous_app);
-
-    program
-      .command('init')
-      .description('create a new superfluous app in the current directory')
-      .action(init_superfluous_app);
-
-    program
-      .command('undo')
-      .description('undo the last superfluous command')
-      .action(function() {
-        console.log('lol. did you really think it would be that easy?');
-      });
 
     program.parse(process.argv);
 
