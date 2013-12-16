748759ce3c2e8e7a31a75613fe2bf37e5efba481
Max Goodman <c@chromakode.com>
2013-12-15 16:08:54 -0800

    Use class selectors to scope component rules.
    Class selectors are faster than attribute selectors, especially in IE.

--- superfluous/core/server/component.js
+++ superfluous/core/server/component.js
@@ -171,6 +171,8 @@ Component.build = function(component, options, cb) {
     className += " " + additionalClasses;
   }
 
+  className += " cmp-" + component;
+
   if (cmp.template) {
     var render_options = _.extend({
       id: id,
