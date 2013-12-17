601c7ff1e59f2e8c93568f1b53e74eaf33f79737
okay <okay@chert>
2013-12-17 14:41:45 -0800

    update to 0.12

--- superfluous/app/main.js
+++ superfluous/app/main.js
@@ -5,5 +5,9 @@ var store = require_core("server/store");
 module.exports = {
   setup: function() {
     console.log("Main setup stuff, something, something");
+  },
+  setup_store: function() {
+    store.set(null);
+    return true;
   }
 };
