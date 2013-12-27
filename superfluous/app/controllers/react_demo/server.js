a305e990e3c5896568653e5fa45e4d77db401078
okay <okay@chert>
2013-12-27 15:06:44 -0800

    update React TODO list items

--- superfluous/app/controllers/react_demo/server.js
+++ superfluous/app/controllers/react_demo/server.js
@@ -5,7 +5,7 @@ var controller = require_core("server/controller");
 // Helpers for serialized form elements
 var value_of = controller.value_of,
     array_of = controller.array_of;
-    
+
 
 var models = require_app("controllers/react_demo/models");
 
@@ -15,16 +15,19 @@ module.exports = {
   },
 
   index: function(ctx, api) {
-    var todos = new models.TodoList([ 
-      { title: "foo", completed: false, editing: false, id: 1 },
-      { title: "baz", completed: true, editing: false, id: 0 }
+    var todos = new models.TodoList([
+      { title: "build a bigger React site", completed: false, editing: false, id: 4 },
+      { title: "sync backbone collection to server", completed: false, editing: false, id: 5 },
+      { title: "tie Backbone & React together", completed: true, editing: false, id: 3 },
+      { title: "add a React Component", completed: true, editing: false, id: 2 },
+      { title: "learn how to use React", completed: true, editing: false, id: 1 },
     ]);
 
     var react_el = global.$R("todomvc", { todos: todos, editing: {} });
     var sidebar = $C("about_sidebar", { client_options: { div: ".contents" }});
     api.template.add_stylesheet("todomvc_global");
     var template_str = api.template.render("controllers/react_demo.html.erb", {
-      todo_el: react_el.toString() 
+      todo_el: react_el.toString()
     });
 
     // rememember to marshall components that don't get rendered!
