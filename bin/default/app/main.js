58dcc5f5c3cdfcd9abfb12732dae4084477e3769
okay <okay@chert>
2014-01-07 10:07:23 -0800

    add slog to default app

--- bin/default/app/main.js
+++ bin/default/app/main.js
@@ -3,6 +3,7 @@
 module.exports = {
   setup_app: function() {
     console.log("Main setup stuff, something, something");
+    require_app("plugins/slog/server").install();
   },
   setup_request: function(req) {
     console.log("Handling request", req.path, req.query, req.params);
