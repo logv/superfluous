12510e01bb446e8a067d359abadf83e53c3602eb
okay <okay@chert>
2014-01-06 21:30:40 -0800

    update home page styling

--- superfluous/app/controllers/home/server.js
+++ superfluous/app/controllers/home/server.js
@@ -7,6 +7,12 @@ module.exports = {
 
   index: function(ctx, api) {
     var template_str = api.template.render("controllers/home.html.erb");
+    var async_work = api.page.async(function(flush) {
+      api.template.add_stylesheet("tomorrow-night-bright");
+      api.bridge.call("app/static/vendor/highlight.pack", "initHighlighting");
+      flush();
+    });
+    async_work();
     api.page.render({ content: template_str});
   },
 
