195ca1c54845283850e9ecab00268add63421386
okay <okay@chert>
2014-03-21 14:53:42 -0700

    make reload on server restart forceable, and catch SIGINT

--- superfluous/core/server/shutdown.js
+++ superfluous/core/server/shutdown.js
@@ -7,7 +7,7 @@ function gracefulShutdown(io, cb) {
   var sockets = socket.get_open_sockets();
 
   // if we are in development mode
-  if (!config.RELEASE) {
+  if (!config.RELEASE || config.REFRESH_ON_RESTART) {
     _.each(sockets, function(socket) {
       socket.emit("__refresh", { auth: parseInt(Math.random() * 120098, 10)});
     });
@@ -22,6 +22,11 @@ module.exports = {
       gracefulShutdown(io, function() { });
     });
 
+    process.once('SIGINT', function () {
+      gracefulShutdown(io, function () {
+        process.kill(process.pid, 'SIGINT'); 
+      });
+    });
     process.once('SIGUSR2', function () {
       gracefulShutdown(io, function () {
         process.kill(process.pid, 'SIGUSR2'); 
