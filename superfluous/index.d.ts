ff14eef56bd50b8eb23a03182e45e16b9801eaa3
Travis Cline <travis.cline@gmail.com>
2018-06-28 22:21:10 -0700

    [types] add basic typescript type definitions

---
+++ superfluous/index.d.ts
@@ -0,0 +1,7 @@
+declare namespace Superfluous {
+  type Bootloader = any;
+}
+declare var bootloader: Superfluous.Bootloader;
+
+declare function require_app(a: string): any;
+declare function require_core(a: string): any;
